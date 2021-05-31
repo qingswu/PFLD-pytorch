3a961dafdee7c55273fedc8f4d983b67026749ef
qingsong <qqsongzi@gmail.com>
2021-05-30 23:21:39 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pfld",
+    description="pfld playground",
+    author="author",
+    author_email="author@email.com",
+    install_requires=[,],
+    version="0.0.1",
+    packages=find_packages(),
+)
