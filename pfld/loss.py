940baa49e79b6a991e84cb3201793b9ce533494a
qingsong <qqsongzi@gmail.com>
2021-06-05 08:05:28 -0400

    removed batchsize arg

--- pfld/loss.py
+++ pfld/loss.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -16,8 +15,8 @@ class PFLDLoss(nn.Module):
         euler_angle_gt,
         angle,
         landmarks,
-        train_batchsize,
     ):
+        train_batchsize = landmark_gt.shape[0]
         weight_angle = torch.sum(1 - torch.cos(angle - euler_angle_gt), axis=1)
         attributes_w_n = attribute_gt[:, 1:6].float()
         mat_ratio = torch.mean(attributes_w_n, axis=0)
@@ -32,29 +31,3 @@ class PFLDLoss(nn.Module):
         return torch.mean(weight_angle * weight_attribute * l2_distant), torch.mean(
             l2_distant
         )
-
-
-def smoothL1(y_true, y_pred, beta=1):
-    """
-    very similar to the smooth_l1_loss from pytorch, but with
-    the extra beta parameter
-    """
-    mae = torch.abs(y_true - y_pred)
-    loss = torch.sum(
-        torch.where(mae > beta, mae - 0.5 * beta, 0.5 * mae ** 2 / beta), axis=-1
-    )
-    return torch.mean(loss)
-
-
-def wing_loss(y_true, y_pred, w=10.0, epsilon=2.0, N_LANDMARK=106):
-    y_pred = y_pred.reshape(-1, N_LANDMARK, 2)
-    y_true = y_true.reshape(-1, N_LANDMARK, 2)
-
-    x = y_true - y_pred
-    c = w * (1.0 - math.log(1.0 + w / epsilon))
-    absolute_x = torch.abs(x)
-    losses = torch.where(
-        w > absolute_x, w * torch.log(1.0 + absolute_x / epsilon), absolute_x - c
-    )
-    loss = torch.mean(torch.sum(losses, axis=[1, 2]), axis=0)
-    return loss
